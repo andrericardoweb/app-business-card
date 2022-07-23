b32f7574017a5bf4335ab7453c6d905d3657377e
andrericardoweb <andrericardoweb@gmail.com>
2022-07-23 10:43:35 -0300

    criacao do layout da main

---
+++ app/src/main/java/dev/andrericardo/businesscard/App.kt
@@ -0,0 +1,6 @@
+package dev.andrericardo.businesscard
+
+import android.app.Application
+
+class App : Application() {
+}
---
+++ app/src/main/java/dev/andrericardo/businesscard/ui/MainActivity.kt
@@ -0,0 +1,17 @@
+package dev.andrericardo.businesscard.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.andrericardo.businesscard.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+    }
+}
