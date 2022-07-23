ea4ac6555d3153df2d9fdaac103bebf1e2517bd5
andrericardoweb <andrericardoweb@gmail.com>
2022-07-23 19:07:37 -0300

    iniciando configuração do room database

--- app/src/main/java/dev/andrericardo/businesscard/App.kt
+++ app/src/main/java/dev/andrericardo/businesscard/App.kt
@@ -1,6 +1,10 @@
 package dev.andrericardo.businesscard
 
 import android.app.Application
+import dev.andrericardo.businesscard.data.AppDatabase
+import dev.andrericardo.businesscard.data.BusinessCardRepository
 
 class App : Application() {
+    val database by lazy { AppDatabase.getDatabase(this) }
+    val repository by lazy { BusinessCardRepository(database.businessDao()) }
 }
